0e7ea588d55d710e6da98d7be22c34d4d7e2c477
Victor Masson <victormasson21@gmail.com>
2019-07-22 14:57:08 +0100

    Step 3: Sending first response back from server

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var http = require('http')
+var http = require('http');
 
-var server = http.createServer();
+var message = 'I am so happy to be part of the node girls workshop';
+
+function handler (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message);
+  response.end();
+
+}
+
+
+var server = http.createServer(handler);
 
 server.listen(3000, function() {
 
