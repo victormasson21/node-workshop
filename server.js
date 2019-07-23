e968d6981f6c5d7a23b44203bd18aca53d2a8a1d
Victor Masson <victormasson21@gmail.com>
2019-07-23 12:42:11 +0100

    Step 5 Handling different types of file

--- server.js
+++ server.js
@@ -1,35 +1,53 @@
 var http = require('http');
 var fs = require('fs');
 
-function handler (request, response) {
+function handler(request, response) {
   var method = request.method;
   var endpoint = request.url;
 
   if (endpoint === `/`) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-
+    response.writeHead(200, { "Content-Type": "text/html" });
     fs.readFile(__dirname + '/public/index.html', function(error, file) {
       if (error) {
         console.log(error);
         return;
       }
-
       response.end(file);
     });
-/*
-  } else if (endpoint === '/node') {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(`clicked the /node link`);
-    response.end(file);
-  } else if (endpoint === '/girls') {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(`clicked the /girls link`);
-    response.end(file);
+
+  /*
+  } else if (endpoint === `/node`) {
+
+  } else if (endpoint === `/girls`) {
+  */
+
+  } else {
+    // console.log('thats the endpoint: ', endpoint);
+    var extension = endpoint.split('.')[1];
+    function contentType (ext) {
+      switch (ext) {
+          case 'html':
+            return 'text/html';
+          case 'jpg':
+            return'image/jpeg';
+          case 'css':
+            return'text/css';
+          case 'js':
+            return'text/javascript';
+          case 'ico':
+            return'image/x-icon';
+          }
+        }
+
+    response.writeHead(200, { "Content-Type": contentType(extension) });
+    fs.readFile(__dirname + '/public/' + endpoint, function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
   }
-*/
-
-}
-
 }
 
 
